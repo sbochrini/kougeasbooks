3e86d9e0294f943fa653957319703423bc733bcf
Voula <s.bochrini@gmail.com>
2017-07-28 16:50:01 +0300

    hover

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -8,45 +8,45 @@ var check_email=0;
 var has_email=0;
 
 $(function() {
-    $('button.catwithout').on("click", function(){
-        var str=$(this).attr("id");
-        var res = str.split("_");
-        var id =res[1];
+    $('button.catwithout').on('click', function() {
+        let str=$(this).attr('id');
+        let res = str.split('_');
+        let id =res[1];
         $.ajax({
             url: cat_url,
-            type:'GET',
-            data: {id:id},
-            success  : function(response) {
+            type: 'GET',
+            data: {id: id},
+            success: function(response) {
                 $('#mainpage').html(response);
-            }
+            },
         });
     });
 
-    $('button.subcat').on("click", function(){
-        var str=$(this).attr("id");
-        var res = str.split("_");
-        var id =res[1];
+    $('button.subcat').on('click', function() {
+        let str=$(this).attr('id');
+        let res = str.split('_');
+        let id =res[1];
         $.ajax({
             url: sub_url,
-            type:'GET',
-            data: {id:id},
-            success  : function(response) {
+            type: 'GET',
+            data: {id: id},
+            success: function(response) {
                 $('#mainpage').html(response);
-            }
+            },
         });
     });
 
 
-    //get url params
+    // get url params
 
     $.fn._getURLParameter = function getURLParameter(name) {
         return decodeURI(
-            (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+            (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[, null])[1]
         );
-    }
+    };
 
 });
-$('document').ready(function(){
+$('document').ready(function() {
 
     /**
      * user defined functions
@@ -56,34 +56,34 @@ $('document').ready(function(){
         /**
          * PRICE: jquery ui range slider
          */
-        priceSlider: function ($slider, $prev, $next){
+        priceSlider: function($slider, $prev, $next) {
 
             $slider.slider({
                 min: 0,
                 max: 100,
                 range: true,
                 values: [0, 100],
-                slide: function (event, ui){
+                slide: function(event, ui) {
                     $prev.text(ui.values[0]+' €');
                     $next.text(ui.values[1]+' €');
-                }
+                },
             });
         }
 
         /**
          * PRICE: jquery ui set values
          */
-        ,priceValues: function ($slider, $prev, $next){
+        , priceValues: function($slider, $prev, $next) {
 
             $prev.text($slider.slider('values', 0)+' €');
             $next.text($slider.slider('values', 1)+' €');
-        }
+        },
     };
 
     $('#demo').jplist({
         itemsBox: '.list',
         itemPath: '.list-item',
-        panelPath: '.jplist-panel'
+        panelPath: '.jplist-panel',
     });
 
 
@@ -111,64 +111,65 @@ $('document').ready(function(){
             }
         }
     }*/
-    var url =window.location.href;
-    if(url.indexOf("authorcatalog") > -1) {
-        var dec_url=decodeURI(url);
-        var splited = dec_url.split('authorcatalog&letter=');
+    let url =window.location.href;
+    if (url.indexOf('authorcatalog') > -1) {
+        let dec_url=decodeURI(url);
+        let splited = dec_url.split('authorcatalog&letter=');
         if (typeof splited[1] !== 'undefined') {
-            var letter = splited[1];
-            var alphabet = document.getElementsByClassName("alphabet");
-            for(var i = 0; i < alphabet.length; i++ ){
-                if(alphabet[i].textContent === letter ){
+            let letter = splited[1];
+            let alphabet = document.getElementsByClassName('alphabet');
+            for (let i = 0; i < alphabet.length; i++ ) {
+                if (alphabet[i].textContent === letter ) {
                     letter_button=alphabet[i];
                    // alert(letter);
-                    letter_button.style.backgroundColor = "#446f88";
-                    letter_button.style.color = "white";
-                }/*else{
-                    if(alphabet[i].getAttribute("href").indexOf(letter)>-1){
-                        letter_button=alphabet[i];
-                        alert(letter);
-                        letter_button.style.backgroundColor = "#446f88";
-                        letter_button.style.color = "white";
-                    }
-                }*/
+                    letter_button.style.backgroundColor = '#446f88';
+                    letter_button.style.color = 'white';
+                }
             }
         }
     }
 
-    $('.cat_collapse i').on('click',function(){
+    $('.cat_collapse i').on('click', function() {
         $(this).toggleClass('fa-plus fa-minus');
-        //alert("despacito");
+        // alert("despacito");
     });
 
-//---------------hover cat menu------------------\\
-   /* $(".panel-heading").hover(
+// ---------------hover cat menu------------------\\
+    $(".panel-default").hover(
         function() {
-            $('.panel-collapse').collapse('show');
+            var collapse = $(this).find(".panel-collapse");
+            var icon=$(this).find("i");
+            $(collapse).collapse("show");
+            $(icon).removeClass("fa-plus");
+            $(icon).addClass("fa-minus");
         }, function() {
-            $('.panel-collapse').collapse('hide');
+            var collapse = $(this).find(".panel-collapse");
+            var icon=$(this).find("i");
+            $(collapse).collapse("hide");
+            $(icon).removeClass("fa-minus");
+            $(icon).addClass("fa-plus");
         }
-    );*/
+    );
 });
 
-//------------index usr order modal---------------\\
+// ------------index usr order modal---------------\\
 $('#userorderModal').on('show.bs.modal', function(e) {
-    document.getElementById("usr_index_order_form").reset();
-    $(".has-error").removeClass('has-error');
-    $(".help-block").empty();
-    //$("#usr_index_order_form").reset();
-    var $modal = $(this);
-    var bk_id = e.relatedTarget.id;
+    document.getElementById('usr_index_order_form').reset();
+    $('.has-error').removeClass('has-error');
+    $('.help-block').empty();
+    // $("#usr_index_order_form").reset();
+    let $modal = $(this);
+    let bk_id = e.relatedTarget.id;
     $.ajax({
         type: 'POST',
         url: 'index.php?r=site/usrordermodal',
-        data: {bk_id  : bk_id},
+        data: {bk_id: bk_id},
         success: function(data)
         {
             // $modal.find('.modal-content').html(data);
             $modal.find('.modal-body').append(data);
-            //$modal.find('.modal-footer').before(data);
-        }
+            // $modal.find('.modal-footer').before(data);
+        },
     });
 
 });
@@ -179,8 +180,8 @@ $(window).load(function() {
     $('#userindexorderModal').modal('show');
 });
 
-$('.cat_subcat').click(function(){
-    var cat_id=$(this).attr('id');
+$('.cat_subcat').click(function() {
+    let cat_id=$(this).attr('id');
     /*  $.ajax({
      type: 'GET',
      url: 'index.php?r=site/bookspercatwith',
@@ -190,13 +191,13 @@ $('.cat_subcat').click(function(){
      $('#booklist').html(data);
      }
      });*/
-    var url='index.php?r=site/bookspercat&id='+cat_id;
+    let url='index.php?r=site/bookspercat&id='+cat_id;
     // alert(url);
     window.location.href=url;
 
 });
 
-/*yii.confirm = function (message, okCallback, cancelCallback) {
+/* yii.confirm = function (message, okCallback, cancelCallback) {
     swal({
         title: message,
         type: 'warning',
@@ -206,15 +207,15 @@ $('.cat_subcat').click(function(){
     }, okCallback);
 };*/
 
-$('.hvr-grow').hover(function ()
+$('.hvr-grow').hover(function()
 {
-    var a=$(this).find('a');
-    a.css({"color": "white"});
+    let a=$(this).find('a');
+    a.css({'color': 'white'});
 });
 
-$('.hvr-grow').mouseleave(function ()
+$('.hvr-grow').mouseleave(function()
 {
-    var a=$(this).find('a');
-    a.css({"color": "#446f88"});
+    let a=$(this).find('a');
+    a.css({'color': '#446f88'});
 });
 
